aa7ccda699b987a69bef4161a228a73254bad8b2
unknown <drewradley@gmail.com>
2018-09-10 09:47:38 -0700

    friend finder

---
+++ data/friends.js
@@ -0,0 +1,9 @@
+// Data
+var users = [
+  {
+  name:"Bob",
+  photo:"https://img00.deviantart.net/d6ab/i/2013/156/a/3/portrait_of_robert_smith_by_melliarine-d5nwlgk.jpg",
+  scores:[-1,5,2,4,2,1,1,4,4,1,1]
+  }
+];
+module.exports = users;
